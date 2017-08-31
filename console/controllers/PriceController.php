cfd80b2cff93e9096f0386386c8c1ce68781a37f
hungnd1 <hungnd1@vivas.vn>
2017-08-31 09:33:13 +0700

    a

--- console/controllers/PriceController.php
+++ console/controllers/PriceController.php
@@ -283,6 +283,27 @@ class PriceController extends Controller
         }
     }
 
+    public  function actionMigrateLatLong()
+    {
+
+        $station = Station::findAll(['status' => Station::STATUS_ACTIVE]);
+        $url = "https://greencoffee.lizard.net/api/v3/locations/?format=json&code=";
+        foreach ($station as $item) {
+            /** @var $item Station */
+            $arr_detail = $this->callCurl($url . $item->station_code);
+            $value = $arr_detail['results'];
+            if ($value) {
+                if (!$item->longtitude || !$item->latitude) {
+                    $long = $arr_detail['results'][0]['geometry']['coordinates'][0];
+                    $lat = $arr_detail['results'][0]['geometry']['coordinates'][1];
+                    $item->latitude = $lat;
+                    $item->longtitude = $long;
+                    $item->save(false);
+                }
+            }
+        }
+    }
+
     public function actionRunWeather()
     {
 
