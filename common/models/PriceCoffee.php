4fb338f091d236007444b65c7bc5ee704760698e
hungnd1 <hungnd1@vivas.vn>
2017-11-13 16:27:41 +0700

    a

--- common/models/PriceCoffee.php
+++ common/models/PriceCoffee.php
@@ -30,6 +30,8 @@ class PriceCoffee extends \yii\db\ActiveRecord
     const UNIT_VND = 1;
     const UNIT_USD = 2;
 
+    const TYPE_COFFE_TUOI = 'Quả tươi vối';
+    const TYPE_COFFE_CHE = 'Cafe chè';
     const TYPE_COFFEE_A = 'Nhân xô chè';
     const TYPE_COFFEE_V = 'Nhân xô vối';
 
@@ -129,12 +131,28 @@ class PriceCoffee extends \yii\db\ActiveRecord
                 ->andWhere(['<=', 'price_coffee.created_at', $to_time + 7 * 60 * 60])
                 ->andWhere(['not in', 'price_coffee.organisation_name', ['dRBE', 'dRCL', 'dACN']])
                 ->orderBy(['price_coffee.province_id' => SORT_DESC])->all();
+            if(!$pricePre){
+                $pricePre = \api\models\PriceCoffee::find()
+                    ->innerJoin('station', 'station.station_code = price_coffee.province_id')
+                    ->andWhere(['station.province_id' => $province_id])
+                    ->andWhere(['>=', 'price_coffee.created_at', $from_time + 7 * 60 * 60 - 86400])
+                    ->andWhere(['<=', 'price_coffee.created_at', $to_time + 7 * 60 * 60 - 86400])
+                    ->andWhere(['not in', 'price_coffee.organisation_name', ['dRBE', 'dRCL', 'dACN']])
+                    ->orderBy(['price_coffee.province_id' => SORT_DESC])->all();
+            }
         } else {
             $pricePre = \api\models\PriceCoffee::find()
                 ->andWhere(['in', 'price_coffee.organisation_name', ['dRBE', 'dRCL', 'dACN']])
                 ->andWhere(['>=', 'price_coffee.created_at', $from_time + 7 * 60 * 60])
                 ->andWhere(['<=', 'price_coffee.created_at', $to_time + 7 * 60 * 60])
                 ->orderBy(['price_coffee.province_id' => SORT_ASC])->all();
+            if(!$pricePre){
+                $pricePre = \api\models\PriceCoffee::find()
+                    ->andWhere(['in', 'price_coffee.organisation_name', ['dRBE', 'dRCL', 'dACN']])
+                    ->andWhere(['>=', 'price_coffee.created_at', $from_time + 7 * 60 * 60 - 86400])
+                    ->andWhere(['<=', 'price_coffee.created_at', $to_time + 7 * 60 * 60 - 86400])
+                    ->orderBy(['price_coffee.province_id' => SORT_ASC])->all();
+            }
         }
 
         return $pricePre;
@@ -175,37 +193,37 @@ class PriceCoffee extends \yii\db\ActiveRecord
                 break;
             case 'dACA':
                 return [
-                    'name_coffee' => PriceCoffee::TYPE_COFFEE_A,
+                    'name_coffee' => PriceCoffee::TYPE_COFFE_CHE,
                     'company' => PriceCoffee::COMPANY_AGENT
                 ];
                 break;
             case 'dACC':
                 return [
-                    'name_coffee' => PriceCoffee::TYPE_COFFEE_A,
+                    'name_coffee' => PriceCoffee::TYPE_COFFE_CHE,
                     'company' => PriceCoffee::COMPANY_COMPANY
                 ];
                 break;
             case 'dACN':
                 return [
-                    'name_coffee' => PriceCoffee::TYPE_COFFEE_A,
+                    'name_coffee' => PriceCoffee::TYPE_COFFE_CHE,
                     'company' => PriceCoffee::COMPANY_COMPANY
                 ];
                 break;
             case 'dRCL':
                 return [
-                    'name_coffee' => PriceCoffee::TYPE_COFFEE_A,
+                    'name_coffee' => PriceCoffee::TYPE_COFFE_CHE,
                     'company' => PriceCoffee::COMPANY_COMPANY
                 ];
                 break;
             case 'dACE':
                 return [
-                    'name_coffee' => PriceCoffee::TYPE_COFFEE_A,
+                    'name_coffee' => PriceCoffee::TYPE_COFFE_TUOI,
                     'company' => PriceCoffee::COMPANY_EXPORT
                 ];
                 break;
             case 'dACF':
                 return [
-                    'name_coffee' => PriceCoffee::TYPE_COFFEE_A,
+                    'name_coffee' => PriceCoffee::TYPE_COFFE_TUOI,
                     'company' => PriceCoffee::COMPANY_FARM_GATE
                 ];
                 break;
@@ -235,25 +253,25 @@ class PriceCoffee extends \yii\db\ActiveRecord
                 break;
             case 'dRCA':
                 return [
-                    'name_coffee' => PriceCoffee::TYPE_COFFEE_V,
+                    'name_coffee' => PriceCoffee::TYPE_COFFE_TUOI,
                     'company' => PriceCoffee::COMPANY_AGENT
                 ];
                 break;
             case 'dRCC':
                 return [
-                    'name_coffee' => PriceCoffee::TYPE_COFFEE_V,
+                    'name_coffee' => PriceCoffee::TYPE_COFFE_TUOI,
                     'company' => PriceCoffee::COMPANY_COMPANY
                 ];
                 break;
             case 'dRCE':
                 return [
-                    'name_coffee' => PriceCoffee::TYPE_COFFEE_V,
+                    'name_coffee' => PriceCoffee::TYPE_COFFE_TUOI,
                     'company' => PriceCoffee::COMPANY_EXPORT
                 ];
                 break;
             case 'dRCF':
                 return [
-                    'name_coffee' => PriceCoffee::TYPE_COFFEE_V,
+                    'name_coffee' => PriceCoffee::TYPE_COFFE_TUOI,
                     'company' => PriceCoffee::COMPANY_FARM_GATE
                 ];
                 break;
