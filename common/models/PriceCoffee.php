365c39a3f6d0d1797b7983ff6af841885ed26acd
hungnd1 <hungnd1@vivas.vn>
2017-11-13 17:01:41 +0700

    a

--- common/models/PriceCoffee.php
+++ common/models/PriceCoffee.php
@@ -33,6 +33,7 @@ class PriceCoffee extends \yii\db\ActiveRecord
     const TYPE_COFFE_TUOI = 'Quả tươi vối';
     const TYPE_QUA_TUOI = 'Quả tươi chè';
     const TYPE_COFFE_CHE = 'Cafe chè';
+    const TYPE_COFFE_VOI = 'Cafe chè';
     const TYPE_COFFEE_A = 'Nhân xô chè';
     const TYPE_COFFEE_V = 'Nhân xô vối';
 
@@ -212,7 +213,7 @@ class PriceCoffee extends \yii\db\ActiveRecord
                 break;
             case 'dRCL':
                 return [
-                    'name_coffee' => PriceCoffee::TYPE_COFFE_CHE,
+                    'name_coffee' => PriceCoffee::TYPE_COFFE_VOI,
                     'company' => PriceCoffee::COMPANY_COMPANY
                 ];
                 break;
