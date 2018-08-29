43df1398c5b5faca71c71e6d7cb2d4a2f8bd86e1
hungnd1 <hungnd1292@gmail.com>
2018-08-29 15:46:54 +0700

    a

--- backend/web/notify_question.sh
+++ backend/web/notify_question.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 id=$1
 echo "Start answer"
-//usr/bin/php /opt/code/motobike/yii question/notify-question $id
+/usr/bin/php /opt/code/motobike/yii question/notify-question $id
 echo "Add answer"
