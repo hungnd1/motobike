6c61009ee7700736da6642a40f995ae81b6fb092
hungnd1 <hungnd1292@gmail.com>
2019-08-29 17:57:54 +0700

    a

--- backend/controllers/CompanyController.php
+++ backend/controllers/CompanyController.php
@@ -205,64 +205,63 @@ class CompanyController extends Controller
                 if ($fileCompanyProfile->saveAs(Yii::getAlias('@webroot') . "/" . Yii::getAlias('@excel_folder') . "/" . $file_name)) {
                     $model->file_company_file = $file_name;
                     $model->save();
-                    $message = shell_exec("nohup  ./upload_company_profile.sh $model->id > /dev/null 2>&1 &");
+//                    $message = shell_exec("nohup  ./upload_company_profile.sh $model->id > /dev/null 2>&1 &");
 //                    ini_set('memory_limit', '-1');
-//                    $objPHPExcel = PHPExcel_IOFactory::load(Yii::getAlias('@excel_folder') . "/" . $file_name);
-//                    $sheetData = $objPHPExcel->getActiveSheet()->toArray(null, true, true, true);
+                    $objPHPExcel = PHPExcel_IOFactory::load(Yii::getAlias('@excel_folder') . "/" . $file_name);
+                    $sheetData = $objPHPExcel->getActiveSheet()->toArray(null, true, true, true);
                     $first = 0;
-//                    if (sizeof($sheetData) > 0) {
-//                        foreach ($sheetData as $row) {
-//                            $first ++ ;
-//                            if($first > 3){
-//                                $companyProfile = new CompanyProfile();
-//                                var_dump($row['B']);exit;
-//                                $companyProfile->ma = $row['B'];
-//                                $companyProfile->cmnd = $row['C'];
-//                                $companyProfile->ten = $row['D'];
-//                                $companyProfile->ho = $row['E'];
-//                                $companyProfile->gioi_tinh = $row['F'];
-//                                $companyProfile->thon_lang = $row['G'];
-//                                $companyProfile->huyen = $row['H'];
-//                                $companyProfile->thanh_pho = $row['I'];
-//                                $companyProfile->sdt = $row['J'];
-//                                $companyProfile->email = $row['K'];
-//                                $companyProfile->nam_sinh = $row['L'];
-//                                $companyProfile->so_giay_to_chung_nhan = $row['M'];
-//                                $companyProfile->vi_do_gps = $row['N'];
-//                                $companyProfile->kinh_do_gps = $row['O'];
-//                                $companyProfile->ten_nguoi_dung = $row['P'];
-//                                $companyProfile->loai_ca_phe = $row['Q'];
-//                                $companyProfile->tong_san_luong_nam_nay = $row['R'];
-//                                $companyProfile->tong_san_luong_nam_ngoai = $row['S'];
-//                                $companyProfile->san_luong_ban_giao_nam_ngoai = $row['T'];
-//                                $companyProfile->san_luong_ban_giao_2_nam_truoc = $row['U'];
-//                                $companyProfile->san_luong_ban_giao_3_nam_truoc = $row['V'];
-//                                $companyProfile->nguoi_cmnd = $row['W'];
-//                                $companyProfile->nguoi_ten = $row['X'];
-//                                $companyProfile->nguoi_ho = $row['Y'];
-//                                $companyProfile->nguoi_gioi_tinh = $row['Z'];
-//                                $companyProfile->nguoi_email = $row['AA'];
-//                                $companyProfile->nguoi_sdt = $row['AB'];
-//                                $companyProfile->thanh_vien_nhom = $row['AC'];
-//                                $companyProfile->chung_nhan_tu_nam = $row['AD'];
-//                                $companyProfile->chuong_trinh_chung_nhan_khac = $row['AE'];
-//                                $companyProfile->chung_nhan_khac = $row['AF'];
-//                                $companyProfile->cmnd_thanh_tra = $row['AG'];
-//                                $companyProfile->hoten_thanh_tra = $row['AH'];
-//                                $companyProfile->nam_thanh_tra = $row['AI'];
-//                                $companyProfile->thang_thanh_tra = $row['AJ'];
-//                                $companyProfile->ngay_thanh_tra = $row['AK'];
-//                                $companyProfile->sl_cong_nhan_thoi_vu = $row['AL'];
-//                                $companyProfile->sl_cong_nhan_dai_han = $row['AM'];
-//                                $companyProfile->tong_so_vuon_ca_phe = $row['AN'];
-//                                $companyProfile->tong_so_dien_tich_chung_nhan = $row['AO'];
-//                                $companyProfile->tong_so_dien_tich_cac_vuon = $row['AP'];
-////                                $companyProfile->id_number = $row['AQ'];
-//                                $companyProfile->id_company = $model->id;
-////                                $companyProfile->save(false);
-//                            }
-//                        }
-//                    }
+                    if (sizeof($sheetData) > 0) {
+                        foreach ($sheetData as $row) {
+                            $first ++ ;
+                            if($first > 5){
+                                $companyProfile = new CompanyProfile();
+                                $companyProfile->ma = $row['B'];
+                                $companyProfile->cmnd = $row['C'];
+                                $companyProfile->ten = $row['D'];
+                                $companyProfile->ho = $row['E'];
+                                $companyProfile->gioi_tinh = $row['F'];
+                                $companyProfile->thon_lang = $row['G'];
+                                $companyProfile->huyen = $row['H'];
+                                $companyProfile->thanh_pho = $row['I'];
+                                $companyProfile->sdt = $row['J'];
+                                $companyProfile->email = $row['K'];
+                                $companyProfile->nam_sinh = $row['L'];
+                                $companyProfile->so_giay_to_chung_nhan = $row['M'];
+                                $companyProfile->vi_do_gps = $row['N'];
+                                $companyProfile->kinh_do_gps = $row['O'];
+                                $companyProfile->ten_nguoi_dung = $row['P'];
+                                $companyProfile->loai_ca_phe = $row['Q'];
+                                $companyProfile->tong_san_luong_nam_nay = $row['R'];
+                                $companyProfile->tong_san_luong_nam_ngoai = $row['S'];
+                                $companyProfile->san_luong_ban_giao_nam_ngoai = $row['T'];
+                                $companyProfile->san_luong_ban_giao_2_nam_truoc = $row['U'];
+                                $companyProfile->san_luong_ban_giao_3_nam_truoc = $row['V'];
+                                $companyProfile->nguoi_cmnd = $row['W'];
+                                $companyProfile->nguoi_ten = $row['X'];
+                                $companyProfile->nguoi_ho = $row['Y'];
+                                $companyProfile->nguoi_gioi_tinh = $row['Z'];
+                                $companyProfile->nguoi_email = $row['AA'];
+                                $companyProfile->nguoi_sdt = $row['AB'];
+                                $companyProfile->thanh_vien_nhom = $row['AC'];
+                                $companyProfile->chung_nhan_tu_nam = $row['AD'];
+                                $companyProfile->chuong_trinh_chung_nhan_khac = $row['AE'];
+                                $companyProfile->chung_nhan_khac = $row['AF'];
+                                $companyProfile->cmnd_thanh_tra = $row['AG'];
+                                $companyProfile->hoten_thanh_tra = $row['AH'];
+                                $companyProfile->nam_thanh_tra = $row['AI'];
+                                $companyProfile->thang_thanh_tra = $row['AJ'];
+                                $companyProfile->ngay_thanh_tra = $row['AK'];
+                                $companyProfile->sl_cong_nhan_thoi_vu = $row['AL'];
+                                $companyProfile->sl_cong_nhan_dai_han = $row['AM'];
+                                $companyProfile->tong_so_vuon_ca_phe = $row['AN'];
+                                $companyProfile->tong_so_dien_tich_chung_nhan = $row['AO'];
+                                $companyProfile->tong_so_dien_tich_cac_vuon = $row['AP'];
+//                                $companyProfile->id_number = $row['AQ'];
+                                $companyProfile->id_company = $model->id;
+                                $companyProfile->save(false);
+                            }
+                        }
+                    }
                 }
             }
 
